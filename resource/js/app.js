da69a78700e7fdc2fcdb13af52e9bee6f7885546
Xiaojing <8redfishcreative@gmail.com>
2022-09-02 10:40:11 -0400

    first page and style

---
+++ resource/js/app.js
@@ -0,0 +1,31 @@
+
+
+
+//The init function is called when the page loads
+funciton Infinity(){
+
+
+}
+
+// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+function startTimer() {
+  // Sets timer
+  timer = setInterval(function() {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (timerCount >= 0) {
+      // Tests if win condition is met
+      if (isWin && timerCount > 0) {
+        // Clears interval and stops timer
+        clearInterval(timer);
+        winGame();
+      }
+    }
+    // Tests if time has run out
+    if (timerCount === 0) {
+      // Clears interval
+      clearInterval(timer);
+      loseGame();
+    }
+  }, 1000);
+}
