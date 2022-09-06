d97f790d8091f06ca1a8b96dac677cadf8f4054a
Xiaojing <8redfishcreative@gmail.com>
2022-09-05 21:32:27 -0400

    show all questions and anwers and correction

--- resource/js/app.js
+++ resource/js/app.js
@@ -31,7 +31,7 @@ var questionIndex = 0;
 var score = 0;
 var questionContent = document.getElementById("question_content");
 
-var ul = document.getElementById("ul");
+var ol = document.getElementById("ol");
 var start = document.getElementById("start_btn");
 var leftTime = document.getElementsByClassName(
 "left_time");
@@ -61,44 +61,70 @@ start.addEventListener("click", function(){
 function loadQuestion(questionIndex){
 //clear exsiting data
 questionContent.innerHTML=""
-ul.innerHtml = "";
-
+ol.innerHtml = "";
 //for loop throng the array
 for(var i=0; i<questions.length; i++){
 //append title
 var userQuestion = questions[questionIndex].title;
-
 var userOptions = questions[questionIndex].choices;
-
 }
 //create question title
 let questionTitle = document.createElement("div");
 questionTitle.setAttribute("id","questionTitle");
 questionTitle.textContent = userQuestion;
 questionContent.appendChild(questionTitle);
-
-
-
+//before append list,clear ol
+ol.innerText="";
+//create answers list
  userOptions.forEach((item)=>{
-  let li = document.createElement("li");
+  var li = document.createElement("li");
 //set li's styles
   li.setAttribute("id","li");
-  li.innerText = item;
-  questionContent.appendChild(ul);
-  ul.appendChild(li);
+  li.textContent = item;
+
+  questionContent.appendChild(ol);
+  ol.appendChild(li);
+
+li.addEventListener("click",checkAnswer);
 })
-
-  
-
+//click li Triggers checkAnswer funtion
 
 
 }
 //compare choice  and answer
-var checkAnswer = function(){
+function checkAnswer(e){
 
+if(questionIndex < questions.length){
+
+correction(e.target.innerText === questions[questionIndex].answer);
+
+}else{
+allDone();
+}
 }
 
-// add eventlistener click start button
+//add correction function
+function correction(condition){
+//first rend question,then show correction
+questionIndex++;
+
+loadQuestion(questionIndex);
+
+var correctOrWrong = document.createElement("div");
+var creatLine = document.createElement("div");
+creatLine.textContent = "";
+correctOrWrong.setAttribute("id","correctOrWrong");
+creatLine.setAttribute("id","line");
+if(condition){
+correctOrWrong.textContent = "Correct!";
+}else{
+correctOrWrong.textContent = "Wrong!";
+leftTime = leftTime - penalty ;
+}
+questionContent.appendChild(creatLine);
+questionContent.appendChild(correctOrWrong);
+}
+
 
 //add gameover funtion
 var allDone = function(){
